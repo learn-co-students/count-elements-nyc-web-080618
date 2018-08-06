7a056012bf37d18032b762b667ecc1a6ef3c4e33
github username <github email address>
2018-08-06 18:02:44 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
 def count_elements(array)
-  # code goes here
+  return_hash = {}
+  array.each do |animal|
+    if return_hash[animal] != nil
+      return_hash[animal] += 1
+    else
+      return_hash[animal] = 1
+    end
+
+  end
+
+  return_hash
+
 end
- 
