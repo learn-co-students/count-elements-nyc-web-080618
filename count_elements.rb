d619f85b15639a2ae55755a7d03f5fd8c521a5d6
madtab7 <madtab7@gmail.com>
2018-08-06 18:48:05 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |element|
+    #add value / how many times element appears to hash
+    if new_hash[element] == nil
+      new_hash[element]=1
+    else
+      new_hash[element] += 1
+    end
+  end
+  new_hash
 end
- 
