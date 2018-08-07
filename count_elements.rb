991b481f9fdf6e844a0ec294e11159559add1903
Yan Wang <Yan@Flatirons-Air-4.fios-router.home>
2018-08-07 00:59:43 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  hash = {}
+  array.each do |str|
+    if hash[str]
+      hash[str] += 1
+    else
+      hash[str] = 1
+    end
+  end
+  hash
 end
- 
