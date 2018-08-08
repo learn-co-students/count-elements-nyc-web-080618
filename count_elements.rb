e59b06469ce337326f54362d23e025d10a9d202e
kristinatong <kristina.tong@gmail.com>
2018-08-07 22:49:14 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  count = 0
+  hash = {}
+  array.sort.each do |animal|
+    if hash[animal] == nil
+      hash[animal] = 1
+    else
+      hash[animal] += 1
+    end
+  end
+  hash
 end
- 
