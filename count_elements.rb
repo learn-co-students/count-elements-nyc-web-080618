e6d940775cce8c1e3c2fbd3c4a8590255a5152ea
mguevara717 <marissa.guevara717@gmail.com>
2018-08-11 18:18:31 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
+require "pry"
 def count_elements(array)
-  # code goes here
+  animal_hash = {}
+
+  array.sort.each do |words|
+    if animal_hash[words] == nil #checking if empty?
+    #add to the hash from the array and count
+    animal_hash[words] = 1
+    else
+      animal_hash[words] += 1
+      #binding.pry
+    end
 end
- 
+  animal_hash
+  #binding.pry
+end
